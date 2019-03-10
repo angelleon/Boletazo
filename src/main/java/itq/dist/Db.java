49e6e8a6cc04f28dd7dbee531539d3d369792696
Angel Leon <luianglenlop@gmail.com>
2019-03-10 12:49:00 -0600

    open connection to msqy at constructor, added basic getEventsAt signature to search events by date

--- src/main/java/itq/dist/Db.java
+++ src/main/java/itq/dist/Db.java
@@ -1,21 +1,39 @@
-import java.lang.jdbc;
+package itq.dist;
+
+import java.sql.*;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 public class Db
 {
-	private static final String usr = "boletazodev";
-	private static final String passwd = "contrapass";
-	private conn;
-	
-	Db()
-	{
-		try
-		{
-			
-		}
-		catch (SqlException e)
-		{
-			log.error("An error occurred when trying to connect to DB");
-			log.error(e.getMessage());
-		}
-	}
+    private static final Logger log = LogManager.getLogger(Db.class);
+    private static final String USR = "boletazodev";
+    private static final String PASSWD = "contrapass";
+    private static final String URL = "jdbc:mysql://localhost:3306/Boletazo";
+
+    private Connection conn;
+
+    Db()
+    {
+        try
+        {
+            conn = DriverManager.getConnection(URL, USR, PASSWD);
+        }
+        catch (SQLException e)
+        {
+            log.error("An error occurred when trying to connect to DB");
+            log.error(e.getMessage());
+        }
+    }
+
+    /**
+     * 
+     * @param date:
+     * @return An array with all events that ocurr at certain date
+     */
+    public Event[] getEventsAt(Date date)
+    {
+
+    }
 }
