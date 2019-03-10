9a6b0a76e26d31712404c5c9a916e3ac9601ff16
Angel Leon <luianglenlop@gmail.com>
2019-03-10 14:22:59 -0600

    added sql statement to retrive events

--- src/main/java/itq/dist/Db.java
+++ src/main/java/itq/dist/Db.java
@@ -1,6 +1,9 @@
 package itq.dist;
 
 import java.sql.*;
+//import java.sql.Date;
+import java.util.Calendar;
+import java.util.Date;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
@@ -11,6 +14,8 @@ public class Db
     private static final String USR = "boletazodev";
     private static final String PASSWD = "contrapass";
     private static final String URL = "jdbc:mysql://localhost:3306/Boletazo";
+    private static final String SELECT_EVENT_BETWEEN_DATES = "SELECT * FROM Events WHERE date BETWEEN ? AND ?";
+    private static char mander = 'c';
 
     private Connection conn;
 
@@ -32,8 +37,43 @@ public class Db
      * @param date:
      * @return An array with all events that ocurr at certain date
      */
-    public Event[] getEventsAt(Date date)
+    public Event[] getEventsAt(java.util.Date date)
     {
+        Event[] events = null;
+        ResultSet result = null;
 
+        Calendar cal = Calendar.getInstance();
+        cal.setTime(date);
+        cal.set(Calendar.MILLISECOND, 0);
+        cal.set(Calendar.SECOND, 0);
+        cal.set(Calendar.MINUTE, 0);
+        cal.set(Calendar.HOUR, 0);
+
+        long t0 = cal.getTimeInMillis();
+
+        cal.add(Calendar.HOUR, 23);
+        cal.add(Calendar.MINUTE, 59);
+        cal.add(Calendar.SECOND, 59);
+        cal.add(Calendar.MILLISECOND, 999);
+
+        long t1 = cal.getTimeInMillis();
+
+        try
+        {
+            PreparedStatement ps = conn.prepareStatement(SELECT_EVENT_BETWEEN_DATES);
+            ps.setDate(1, new java.sql.Date(t0));
+            ps.setDate(2, new java.sql.Date(t1));
+            result = ps.executeQuery();
+        }
+        catch (SQLException e)
+        {
+        }
+        
+        if (result != null)
+        {
+            result.
+        }
+
+        return events;
     }
 }
