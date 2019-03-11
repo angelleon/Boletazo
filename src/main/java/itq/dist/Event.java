badfbb2dbe57b5f7cd5a9d7df7051792147c8b97
Angel Leon <luianglenlop@gmail.com>
2019-03-10 23:10:28 -0600

    added event attributes, added constructor

--- src/main/java/itq/dist/Event.java
+++ src/main/java/itq/dist/Event.java
@@ -1,15 +1,32 @@
 package itq.dist;
 
+import java.time.LocalDate;
+
 public class Event
 {
+    private int idEvent;
+    private String name;
+    private String description;
+    private LocalDate date;
+    private int idVenue;
+
     Event()
     {
+    }
 
+    Event(int idEvent, String name, String description, LocalDate date, int idVenue)
+    {
+        this.idEvent = idEvent;
+        this.name = name;
+        this.description = description;
+        this.date = date;
+        this.idVenue = idVenue;
     }
 
     @Override
     public String toString()
     {
-        return "Event object";
+        return "Event object {idEvent: " + idEvent + ", name: " + name + ", description: " + description + ", date: "
+                + date.toString() + ", idVenue: " + idVenue + "}";
     }
 }
