cd771604e516fbe3e5b011047c916cc53eac4e13
Angel Leon <luianglenlop@gmail.com>
2019-03-21 12:21:34 -0600

    Added escential attributes for ticket class

--- src/main/java/itq/dist/Boleto.java
+++ src/main/java/itq/dist/Boleto.java
@@ -2,5 +2,48 @@ package itq.dist;
 
 public class Boleto
 {
+    private int idTicket;
+    private String seatNumber;
+    private int idStatus;
+    private int idSection;
+    private int idEvent;
 
+    Boleto(int idTicket, String seatNumber, int idStatus, int idSection, int idEvent)
+    {
+        this.idTicket = idTicket;
+        this.seatNumber = seatNumber;
+        this.idStatus = idStatus;
+        this.idSection = idSection;
+        this.idEvent = idEvent;
+    }
+
+    Boleto()
+    {
+        this(0, "a0", 0, 0, 0);
+    }
+
+    public int getIdTicket()
+    {
+        return idTicket;
+    }
+
+    public String getSeatNumber()
+    {
+        return seatNumber;
+    }
+
+    public int getIdStatus()
+    {
+        return idStatus;
+    }
+
+    public int getIdSection()
+    {
+        return idSection;
+    }
+
+    public int getIdEvent()
+    {
+        return idEvent;
+    }
 }
