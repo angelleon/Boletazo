a8849ee9a9161d0d97cda6880e200899650c489a
magand <magand07@gmail.com>
2019-03-24 20:55:40 -0600

    update step 8 - 15

--- src/main/java/itq/dist/SocketThread.java
+++ src/main/java/itq/dist/SocketThread.java
@@ -21,7 +21,15 @@ public class SocketThread extends Thread
     private SessionControl sc;
     private int sessionId;
     private StringBuilder msgOut;
-
+    
+    private int nRequestedTickets;
+    private int idEvent;
+    private int[] tickets_array;
+    private String usr;
+	private String pass;
+	private String email;
+	private String stateResidence;
+	
     private static enum STATE {
         C_START_SESSION,
         S_START_SESSION,
@@ -122,7 +130,7 @@ public class SocketThread extends Thread
         currentState = STATE.S_START_SESSION;
         msgOut.setLength(0);
         int sessionId = sc.getNewSessionId();
-        if (sessionId > 0)
+        if (sessionId > 0) // #ArmaTuMensaje
         {
             msgOut.append(STATE.S_START_SESSION.ordinal());
             msgOut.append(sessionId);
@@ -169,52 +177,190 @@ public class SocketThread extends Thread
         return false;
     }
 
-    //
+    //opCode = 8 
     private boolean requestReserveTickets()
             throws ConversationException, IOException
     {
+    	currentState =STATE.REQUEST_RESERVE_TICKETS;
+    	//msgOut.setLength(0);   
+    	String rawMsg = dataIn.readUTF();
+    	
+    	if(checkMsgIntegrity(rawMsg) && sessionId >0){
+    		String[] parts = rawMsg.split(",");
+    		int opcode = Integer.parseInt(parts[0]);
+    		sessionId = Integer.parseInt(parts[1]);
+    		idEvent = Integer.parseInt(parts[2]);
+    		// ver que el nRequestedTickets sea menor o igual que el numero de tickets permitidos a comprar
+    		nRequestedTickets = Integer.parseInt(parts[3]);
+    		
+    		tickets_array = new int[nRequestedTickets];
+    		//desde la posicion de nRequestedTicked + nRequestTicked
+    		
+    		int numPart = 2;
+    		//array con los tickets solicitados
+    		for(int i = 0;i<nRequestedTickets;i++) {
+    			numPart++;
+    			tickets_array[i] = Integer.parseInt(parts[numPart]);
+    			
+    		}
+    		//debe comenzar aca el contador del tiempo?
+    		//consultar cada ticket 
+    		
+    		return true;
+    	}
         return false;
     }
 
     private boolean confirmReserveTickets()
             throws ConversationException, IOException
     {
+    	currentState =STATE.CONFIRM_RESERVE_TICKETS;
+    	msgOut.setLength(0);
+    	 if (sessionId > 0) // #ArmaTuMensaje
+         {
+             msgOut.append(currentState);
+             msgOut.append(sessionId);
+             float cost = 0;
+             ///.............Calcular el  costo...e_é
+             
+             
+             msgOut.append(cost);
+             msgOut.append(nRequestedTickets);
+             
+             
+             dataOut.writeUTF(msgOut.toString());
+             return true;
+         }
+    	
         return false;
     }
-
+   
     private boolean singup() throws ConversationException, IOException
     {
+    	currentState =STATE.SINGUP;
+    	String rawMsg = dataIn.readUTF();
+    	if(checkMsgIntegrity(rawMsg)){
+    		String[] parts = rawMsg.split(",");
+    		//int opcode = Integer.parseInt(parts[0]);
+    		sessionId = Integer.parseInt(parts[1]);
+    		 usr = parts[2];
+    		 pass = parts[3];
+    		 email = parts[4];
+    		 stateResidence = parts[5];
+    		 if(db.singup(email)){
+    			 log.info("usuario registrado: "+usr+" - "+email);
+    			return true;
+    		 }
+    		 log.error("no se puede registrar "+rawMsg);
+    	}
         return false;
     }
 
+    /**
+     * Create a new user for Boletazo	 
+     */
     private boolean singupStatus() throws ConversationException, IOException
     {
+    	currentState = STATE.SINGUP_STATUS;
+    	msgOut.setLength(0);
+    	
+    	msgOut.append(currentState);
+		msgOut.append(sessionId);
+    	if(db.toRegister(usr, pass,email,stateResidence)) {
+    		msgOut.append(0);
+    		dataOut.writeUTF(msgOut.toString());
+    		
+    		return true;
+    	}
+    	msgOut.append(1);
+    	dataOut.writeUTF(msgOut.toString());
+    	log.info("usuario equivocado o falta registrar "+usr+" - " +email);	
         return false;
     }
 
     private boolean loginCheck() throws ConversationException, IOException
     {
+    	currentState =STATE.LOGIN_CHECK;
+    	//msgOut.setLength(0);
+    	String rawMsg = dataIn.readUTF();
+    	
+    	if(checkMsgIntegrity(rawMsg) && sessionId >0){
+    		String[] parts = rawMsg.split(",");
+    		int opCode = Integer.parseInt(parts[0]);
+    		sessionId = Integer.parseInt(parts[1]);
+    		usr = parts[2];
+    		pass = parts[3];
+    		if(db.login(usr,pass)) {
+    			return true;
+    		}
+    	}
+    	log.debug(" algo esta mal escrito U_U "+rawMsg);
         return false;
     }
 
     private boolean loginStatus() throws ConversationException, IOException
     {
+    	currentState =STATE.LOGIN_STATUS;
+    	msgOut.setLength(0);
+    	msgOut.append(currentState);
+		msgOut.append(sessionId);
+		if(db.login(usr, pass)) {
+			msgOut.append("0");
+			dataOut.writeUTF(msgOut.toString());
+			return true;
+		}
+		msgOut.append("1");
+		dataOut.writeUTF(msgOut.toString());
         return false;
     }
+    /** 14)
+     * Client confirm that he wants the tickets
+     * @return 
+     * @throws ConversationException
+     * @throws IOException
+     */
 
     private boolean postPaymentInfo() throws ConversationException, IOException
     {
         currentState = STATE.POST_PAYMENT_INFO;
-
         String rawMsg = dataIn.readUTF();
-        String[] valuesIn = rawMsg.split(",");
-        int sessionId = Integer.parseInt(valuesIn[1]);
+    	
+    	if(checkMsgIntegrity(rawMsg) && sessionId >0){
+    		String[] parts = rawMsg.split(",");
+    		int opcode = Integer.parseInt(parts[0]);
+    		sessionId = Integer.parseInt(parts[1]);
+    		String numberCard = parts[2];
+    		if(numberCard.length()==20) {
+    			String date = parts[3];
+    			String cvv = parts[4];
+    			String type = parts[5];
+    			if(type.equals("VISA")||type.equals("MASTERCARD")) {
+    				log.info("Compra por :"+numberCard+","+date+","+cvv+","+type);
+    				return true;
+    			}
+    			log.error("tipo tarjeta incorrecta ");
+    		}
+    		log.error("longuitud tarjeta incorrecta ");
+    	}
         return false;
     }
 
     private boolean pucharaseCompleted()
             throws ConversationException, IOException
     {
+    	currentState =STATE.PUCHARASE_COMPLETED;
+    	msgOut.setLength(0);
+    	
+    	msgOut.append(currentState);
+		msgOut.append(sessionId);
+		msgOut.append(nRequestedTickets);
+		msgOut.append("el arreglo de los tickets....");
+		if(db.update_ticket_status(tickets_array)) {
+			msgOut.append("0");
+			return true;
+		}
+		msgOut.append("1");
+		dataOut.writeUTF(msgOut.toString());
         return false;
     }
 
